20c8ed019f74314c8e75dc15c92210a32bcc2e7a
SoftwareRat <softwarerat@debian-BULLSEYE-live-builder-AMD64>
2021-12-31 14:38:45 +0100

    Removed Setup Script

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FIREFOX=/usr/lib/firefox
-if [ -d "$FIREFOX/defaults"]; then
-	rm -rf $FIREFOX/defaults
-elif [ -d "$FIREFOX/distribution"]; then
-	rm -rf $FIREFOX/distribution
-fi
-
-git clone https://github.com/SoftwareRat/FirefoxForColab.git "/usr/lib/firefox"
